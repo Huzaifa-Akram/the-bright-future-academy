83617f2adbe33aa70d515d793181712c0d02ef86
Huzaifa-Akram <huzaifaakram121@gmail.com>
2025-01-10 16:54:04 +0500

    Updated video size

--- src/components/IntroVideo.jsx
+++ src/components/IntroVideo.jsx
@@ -8,7 +8,7 @@ function IntroVideo() {
         <iframe
           width="560"
           height="315"
-          src="https://www.youtube.com/embed/Ann_XMs-gfc?si=SoAOO3W50cHnayG9"
+          src="https://www.youtube.com/embed/Ann_XMs-gfc?controls=0&modestbranding=1&rel=0&showinfo=0?modestbranding=1"
           title="YouTube video player"
           frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
