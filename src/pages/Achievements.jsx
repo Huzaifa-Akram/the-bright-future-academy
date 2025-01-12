c84cc8af31b5f85857580affcd86f69299c34cdd
Huzaifa-Akram <huzaifaakram121@gmail.com>
2025-01-12 21:04:21 +0500

    Achievement updated

--- src/pages/Achievements.jsx
+++ src/pages/Achievements.jsx
@@ -1,55 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 import "./achievements.css";
 import { useNavigate } from "react-router-dom";
 
-import ach1 from "../assets/ach1.svg";
-import ach2 from "../assets/ach2.svg";
-import ach3 from "../assets/ach3.svg";
-import ach4 from "../assets/ach4.svg";
-import ach5 from "../assets/ach5.svg";
-import ach6 from "../assets/ach6.svg";
-import ach7 from "../assets/ach7.svg";
-import ach8 from "../assets/ach8.svg";
-import ach9 from "../assets/ach9.svg";
+import ach1 from "../assets/ach1.png";
+import ach2 from "../assets/ach2.png";
+import ach3 from "../assets/ach3.png";
+import ach4 from "../assets/ach4.png";
+import ach5 from "../assets/ach5.png";
+import ach6 from "../assets/ach6.png";
+import ach7 from "../assets/ach7.png";
+import ach8 from "../assets/ach8.png";
+import ach9 from "../assets/ach9.png";
 import back from "../assets/back.svg";
 
 function Achievements() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [currentImage, setCurrentImage] = useState("");
+
   const navigate = useNavigate();
+
   const goBack = () => {
     navigate("/");
   };
+
+  const openLightbox = (image) => {
+    setCurrentImage(image);
+    setIsOpen(true);
+  };
+
+  const closeLightbox = () => {
+    setIsOpen(false);
+  };
+
+  const images = [ach1, ach2, ach3, ach4, ach5, ach6, ach7, ach8, ach9];
+
   return (
     <div className="Achievements">
-      <div className="Achievement1">
-        <img src={ach1} alt="Achievement Image" />
-      </div>
-      <div className="Achievement2">
-        <img src={ach2} alt="Achievement Image" />
-      </div>
-      <div className="Achievement3">
-        <img src={ach3} alt="Achievement Image" />
-      </div>
-      <div className="Achievement4">
-        <img src={ach4} alt="Achievement Image" />
-      </div>
-      <div className="Achievement5">
-        <img src={ach5} alt="Achievement Image" />
-      </div>
-      <div className="Achievement6">
-        <img src={ach6} alt="Achievement Image" />
-      </div>
-      <div className="Achievement7">
-        <img src={ach7} alt="Achievement Image" />
-      </div>
-      <div className="Achievement8">
-        <img src={ach8} alt="Achievement Image" />
-      </div>
-      <div className="Achievement9">
-        <img src={ach9} alt="Achievement Image" />
-      </div>
+      {/* Gallery */}
+      {images.map((image, index) => (
+        <div key={index} className={`Achievement${index + 1}`}>
+          <img
+            src={image}
+            alt={`Achievement ${index + 1}`}
+            onClick={() => openLightbox(image)}
+            style={{ cursor: "pointer" }}
+          />
+        </div>
+      ))}
+
+      {/* Back Button */}
       <div className="back">
-        <img src={back} alt="Back Image" onClick={goBack} />
+        <img
+          src={back}
+          alt="Back Image"
+          onClick={goBack}
+          style={{ cursor: "pointer" }}
+        />
       </div>
+
+      {isOpen && (
+        <div className="lightbox" onClick={closeLightbox}>
+          <span className="lightbox-close" onClick={closeLightbox}>
+            ✖
+          </span>
+          <img src={currentImage} alt="Focused" className="lightbox-image" />
+        </div>
+      )}
     </div>
   );
 }
